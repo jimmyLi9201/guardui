c61d135f677a9c3229fa891b753ce29912f1efdc
lancemao <lancemao@users.noreply.github.com>
2022-08-23 09:01:58 +0000

    deploy: d42ad82ce62b8eb6776c4ca50670b51f7ac2af54

---
+++ assets/component_index.md.0c639ab1.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as e,a}from"./app.e623b97b.js";const h=JSON.parse('{"title":"Component","description":"","frontmatter":{},"headers":[],"relativePath":"component/index.md"}'),o={name:"component/index.md"},t=a("",4),p=[t];function l(c,r,i,d,_,m){return e(),s("div",null,p)}const D=n(o,[["render",l]]);export{h as __pageData,D as default};
